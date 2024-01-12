e27fca00f0476f7d9b23550587d7cf672f3fa035
VIPIN PUNDIR <vipinpundirmzn001@gmail.com>
2024-01-12 16:41:34 +0530

    AddScrollTo

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,18 +6,25 @@ import Portfolio from '../Pages/Portfolio';
 // import Blog from '../Pages/Blog';
 import Contact from '../Pages/Contact';
 import { Routes, Route, NavLink } from "react-router-dom";
+import { useWindowScroll } from "@uidotdev/usehooks";
 
 
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [, scrollTo] = useWindowScroll();
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
-  
-  return (
 
+  const handleScrollTo = () =>{
+    if(window.innerWidth < 901 ){
+      scrollTo({ left: 0, top: 540, behavior: "smooth" })}
+    }
+  
+
+  return (
     <div className='header-css'>
       <div className='header-img-css container1' >
       </div>
@@ -35,10 +42,18 @@ const Header = () => {
 
           <div className={`Navbar ${menuOpen ? 'menu-items-open' : 'menu-items'}`}>
             <ul  >
-              <li><NavLink  to="/" onClick={toggleMenu}>Home</NavLink></li>
-              <li><NavLink  to="/Resume"onClick={toggleMenu}>resume</NavLink></li>
-              <li><NavLink  to="/Portfolio" onClick={toggleMenu}>projects</NavLink></li>
-              <li><NavLink  to="/Contact" onClick={toggleMenu}>contact</NavLink></li>
+              <li onClick={() => scrollTo({ left: 0, top: 0, behavior: "smooth" })} >
+                <NavLink to="/" onClick={toggleMenu}>Home</NavLink>
+              </li>
+              <li onClick={handleScrollTo} >
+                <NavLink to="/Resume" onClick={toggleMenu}>resume</NavLink>
+              </li>
+              <li onClick={handleScrollTo} >
+                <NavLink to="/Portfolio" onClick={toggleMenu}>projects</NavLink>
+              </li>
+              <li onClick={handleScrollTo} >
+                <NavLink to="/Contact" onClick={toggleMenu}>contact</NavLink>
+              </li>
               {/* <li><NavLink to="/Blog" >blog</NavLink></li> */}
             </ul>
           </div>
